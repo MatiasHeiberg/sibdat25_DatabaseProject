6de8ea6762fcbbec95e14ca80f6fe3ae4b230388
Lasse Agerskov <lasse.agerskov@gmail.com>
2026-02-27 10:10:19 +0100

    WPF + fixes

--- Project/DatabaseWPF-final/ResourceManagement/MainWindow.xaml.cs
+++ Project/DatabaseWPF-final/ResourceManagement/MainWindow.xaml.cs
@@ -9,21 +9,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ResourceManagement
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadEmployees();
+        }
 
-           
+        private void LoadEmployees()
+        {
+            using var db = new ResourceManagementDBContext();
+            dgItems.ItemsSource = db.Employees.ToList();
+        }
 
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedEmployee = (ResourceManagement.Model.Employee)dgItems.SelectedItem;
+            using var db = new ResourceManagementDBContext();
+            db.Employees.Remove(selectedEmployee);
+            db.SaveChanges();
 
+            dgItems.ItemsSource = db.Employees.ToList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbFirstName.Text) || string.IsNullOrWhiteSpace(tbSurname.Text))
+            {
+                MessageBox.Show("FirstName og Surname has to be filled!");
+                return;
+            }
+
+            using var db = new ResourceManagementDBContext();
+
+            var emp = new ResourceManagement.Model.Employee
+            {
+                FirstName = tbFirstName.Text,
+                Surname = tbSurname.Text,
+                PhoneNumber = tbPhoneNumber.Text,
+                Role = tbRole.Text,
+            };
+
+            db.Employees.Add(emp);
+            db.SaveChanges();
+
+            LoadEmployees();
+        }
+        private void btnCount_Click_1(object sender, RoutedEventArgs e)
+        {
+            using var db = new ResourceManagementDBContext();
+            var count = db.Database.SqlQueryRaw<int>("EXEC GetEmployeeCount").ToList()[0];
+            MessageBox.Show($"Total employees: {count}");
         }
     }
-}
+    }
---
+++ Project/DatabaseWPF-final/ResourceManagement/Migrations/20260226201534_AddIsActiveDefault.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResourceManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+        }
+    }
+}
